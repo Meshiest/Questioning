58c2bd80abc7a36c74484025a50b2d4301604fea
Meshiest <meek.mesh@gmail.com>
2017-12-11 01:46:50 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,154 @@
+const express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+const escape = require('html-escape');
+const _ = require('lodash');
+
+const PORT = 3000;
+const GAME_TIMEOUT = 1000;
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
+});
+
+let userPool = {};
+let ids = 0;
+let readyTimeout;
+let gameActive = false;
+
+function lobbyEmit() {
+  io.emit('lobby', _.map(_.filter(_.values(userPool), u => u.name),
+    u => ({name: u.name, ready: u.ready})));
+}
+
+function startGame() {
+  gameActive = true;
+
+  let questions = _.uniq(_.map(_.filter(userPool, u => u.name), u => u.question));
+  _.each(userPool, user => {
+    user.ready = false;
+    user.socket.emit('questions', _.shuffle(questions));
+  });
+
+  lobbyEmit();
+}
+
+let answerMap = {};
+
+function startGamePhase2() {
+  let answers = _.map(userPool, ({answers, id, name}) => ({
+    answers: _.sortBy(answers, ['question']), id, name
+  }));
+  answers = _.shuffle(answers);
+  
+  let names = [];
+  _.each(answers, (u, i) => {
+    names.push({name: u.name, id: u.id});
+    answerMap[i] = u.id;
+    delete u.name;
+    u.id = i;
+  });
+  
+  names = _.sortBy(names, ['name']);
+
+  _.each(userPool, user => {
+    user.socket.emit('answers', {
+      names,
+      answers: _.shuffle(answers),
+    });
+  });
+  
+  gameActive = false;
+}
+
+
+io.on('connection', socket => {
+  let id = ids++;
+  let user = userPool[id] = {
+    id,
+    socket,
+    name: '',
+    question: '',
+    answers: [],
+    ready: false,
+  };
+  clearTimeout(readyTimeout);
+
+  io.emit('user-count', Object.keys(userPool).length);
+
+  socket.on('init', ({name, question}) => {
+    if(gameActive)
+      return;
+
+    user.name = escape(name);
+    user.question = escape(question);
+    user.ready = false;
+    user.answers = [];
+    clearTimeout(readyTimeout);
+
+    lobbyEmit();
+  });
+
+  socket.on('answers', answers => {
+    if(!gameActive)
+      return;
+
+    user.answers = _.map(answers, ({question, answer}) => ({
+      answer: escape(answer),
+      question,
+    }));
+    user.ready = true;
+    socket.emit('waiting');
+    lobbyEmit();
+
+    if(!_.filter(userPool, u => !u.ready && u.name).length) {
+      clearTimeout(readyTimeout);
+      readyTimeout = setTimeout(startGamePhase2, GAME_TIMEOUT);
+    } else {
+      clearTimeout(readyTimeout);
+    }
+  });
+
+  socket.on('ready', ready => {
+    if(gameActive)
+      return;
+    
+    user.ready = !!ready;
+    lobbyEmit();
+
+    if(!_.filter(userPool, u => !u.ready && u.name).length) {
+      readyTimeout = setTimeout(startGame, GAME_TIMEOUT);
+    } else {
+      clearTimeout(readyTimeout);
+    }
+  });
+
+  socket.on('guess', guess => {
+    let correct = 0;
+    let total = Object.keys(answerMap).length;
+
+    _.each(guess, (id, guess) => {
+      if(answerMap[guess] == id)
+        correct++;
+    });
+
+    socket.emit('score', correct, total);
+  });
+
+  socket.on('disconnect', () => {
+    delete userPool[id];
+    io.emit('user-count', Object.keys(userPool).length);
+    if(gameActive) {    
+      _.each(userPool, u => u.ready = false);
+      lobbyEmit();
+      io.emit('reset');
+      gameActive = false;
+    }
+  })
+});
+
+http.listen(process.env.PORT || PORT, () => {
+  console.log('Listening on *:', PORT);
+});
---
+++ public/app.js
@@ -0,0 +1,195 @@
+let socket;
+let isReady = false;
+const $ = document.querySelector.bind(document);
+
+// Form handler for initial question and name form
+function submitInit(event) {
+  if(event)
+    event.preventDefault();
+
+  let form = event.target;
+
+  socket.emit('init', {
+    name: form.name.value,
+    question: form.question.value,
+  });
+
+  $('#questionForm').className = 'container hidden';
+  $('#lobbyView').className = 'container';
+  $('#readyButton').className = '';
+  isReady = false;
+}
+
+// Form handler for answering other user's questions
+function submitAnswers(event) {
+  if(event)
+    event.preventDefault();
+
+  let form = event.target;
+  let questions = [];
+  for(let i = 0; i < form.numQuestions.value; i++) {
+    questions.push({
+      question: atob(form['question_' + i].value),
+      answer: form['answer_' + i].value,
+    });
+  }
+
+  socket.emit('answers', questions);
+}
+
+// Form handler for guessing which users correspond to answers
+function submitGuess(event) {
+  if(event)
+    event.preventDefault();
+
+  let form = event.target;
+  let users = {};
+  for(let i = 0; i < form.numUsers.value; i++) {
+    users[form['id_' + i].value] = form['guess_' + i].value;
+  }
+
+  socket.emit('guess', users);
+}
+
+// Called when the ready button is pressed
+function toggleReady(event) {
+  let button = $('#readyButton');
+  isReady = !isReady;
+  button.innerHTML = isReady ? 'Not Ready' : 'Ready!';
+  button.className = isReady ? 'not-ready' : 'ready';
+
+  socket.emit('ready', isReady);
+}
+
+function reset() {
+  $('#lobbyView').className = 'container hidden';
+  $('#answerForm').className = 'container hidden';
+  $('#answerView').className = 'container hidden';
+  $('#questionForm').className = 'container';
+  $('#scoreView').className = 'container hidden';
+  $('#readyButton').className = '';
+  toggleReady();
+  if(isReady)
+    toggleReady();
+}
+
+window.addEventListener('load', () => {
+  socket = io();
+
+  // Online counter on the bottom
+  socket.on('user-count', count => {
+    $('#onlineCount').innerHTML = count;
+  });
+
+  // set the view to the default screen
+  socket.on('reset', reset);
+
+  socket.on('score', (correct, total) => {
+    $('#lobbyView').className = 'container hidden';
+    $('#answerForm').className = 'container hidden';
+    $('#answerView').className = 'container hidden';
+    $('#questionForm').className = 'container hidden';
+    $('#scoreView').className = 'container';
+
+    $('#scoreCorrect').innerHTML = correct;
+    $('#scoreTotal').innerHTML = total;
+  });
+
+  // Called when everyone is done answering questions
+  // and players guess who answered what
+  socket.on('answers', ({names, answers}) => {
+    $('#lobbyView').className = 'container hidden';
+    $('#answerForm').className = 'container hidden';
+    $('#answerView').className = 'container';
+    $('#numUsers').value = answers.length;
+
+    let list = $('#answerList');
+    while(list.hasChildNodes())
+      list.removeChild(list.lastChild);
+
+    answers.forEach(({id, answers}, i) => {
+      let div = document.createElement('div');
+      div.className = 'question';
+      div.innerHTML = `
+        <div class="user-field">
+          <input type="hidden" name="id_${i}" value="${id}">
+          <select name="guess_${i}">
+            <option value="">Select a Name</option>
+            ${names.map(({id, name}) => `
+              <option value="${id}">${name}</option>
+            `)}
+          </select>
+          <div class="answers">
+            ${answers.map(({question, answer}) => `
+              <div class="question-pair">
+                <div class="question">${question}</div>
+                <div class="answer">${answer}</div>
+              </div>
+            `).join('')}
+          </div>
+        </div>`;
+      list.appendChild(div);
+    })
+
+  });
+
+  // Called after the user submit answers to the questions
+  socket.on('waiting', () => {
+    $('#lobbyView').className = 'container';
+    $('#readyButton').className = 'hidden';
+    $('#answerForm').className = 'container hidden';
+  });
+
+  // Called when the after everyone readys up in the lobby
+  // each player receives the questions shuffled
+  socket.on('questions', questions => {
+    $('#questionForm').className = 'container hidden';
+    $('#lobbyView').className = 'container';
+    $('#readyButton').className = 'hidden';
+    $('#answerForm').className = 'container';
+    $('#numQuestions').value = questions.length;
+
+    let list = $('#questionList');
+    while(list.hasChildNodes())
+      list.removeChild(list.lastChild);
+
+    questions.forEach((question, i) => {
+      let div = document.createElement('div');
+      div.className = 'question';
+      div.innerHTML = `
+        <div class="input-container">
+          <label for="answer_${i}">${question}</label>
+          <input type="hidden" name="question_${i}" value="${btoa(question)}">
+          <input type="text" name="answer_${i}" id="answer_${i}" placeholder="Answer" required>
+        </div>`;
+      list.appendChild(div);
+    });
+  });
+
+  // Called everytime ready status is updated
+  socket.on('lobby', users => {
+    let list = $('#userList');
+
+    while(list.hasChildNodes())
+      list.removeChild(list.lastChild);
+
+    users.forEach(({name, ready}) => {
+      let div = document.createElement('div');
+      div.className = 'lobby-user ' + (ready ? 'ready' : 'not-ready');
+      div.innerHTML = `
+        <div class="user-name">
+          ${name}
+        </div>
+        <div class="user-ready">
+          ${ready ? '&check;' : '&#x29D6;'}
+        </div>`;
+      list.appendChild(div);
+    });
+  });
+});
+
+window.addEventListener('unload', () => {
+  if(socket && socket.readyState === WebSocket.OPEN) {
+    socket.close();
+  }
+});
